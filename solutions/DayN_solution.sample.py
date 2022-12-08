8c3d5b109da0773cec3fa574d2bd0f136c7c0395
Marc Gavilan <marcgavilangil@gmail.com>
2022-12-08 20:49:16 +0530

    feat: Sample solution

---
+++ solutions/DayN_solution.sample.py
@@ -0,0 +1,24 @@
+from solutions.SolutionBaseClass import AdventSolution
+
+
+class Solution(AdventSolution):
+    def __init__(self):
+        super().__init__(day=-1)
+
+    def create_input(self, number_cases: int):
+        pass
+
+    def provide_example(self):
+        pass
+
+    def reader_parser(self, problem_string):
+        pass
+
+    def part1(self, problem_string):
+        pass
+
+    def part2(self, problem_string) -> int:
+        pass
+
+    def original_length(self):
+        pass
